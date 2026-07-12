4559319e6742fc56dc3cd38578af264ab079541b
Ramez Khaled <ramez.elzoz74@gmail.com>
2026-07-12 17:34:13 +0300

    Solve Codeforces 112A - Petya and Strings

---
+++ Codeforces/112A-Petya-and-Strings.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    for (int i = 0; i < str1.length(); i++) {
+        str1[i] = tolower(str1[i]);
+        str2[i] = tolower(str2[i]);
+    }
+
+    if (str1 < str2) {
+        cout << -1 << "\n";
+    } else if (str1 > str2) {
+        cout << 1 << "\n";
+    } else {
+        cout << 0 << "\n";
+    }
+
+    return 0;
+}
