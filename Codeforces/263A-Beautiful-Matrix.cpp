78f8a0a34b94838cc76b6cfed63e980b5411c45c
Ramez Khaled <ramez.elzoz74@gmail.com>
2026-07-15 01:42:25 +0300

    Solve Codeforces - Sorting (Bubble Sort)

--- Codeforces/263A-Beautiful-Matrix.cpp
+++ Codeforces/263A-Beautiful-Matrix.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     int arr[5][5], oneRow = 0, oneCol = 0;
 
     for (int i=0; i<5; i++) {
---
+++ Codeforces/Sorting.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    bool swapped;
+    cin >> n;
+    int arr[n];
+
+    for (int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+
+    for (int i=0;i<n-1;i++) {
+        swapped = false;
+        for (int j=0;j<n-i-1;j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped) break;
+    }
+
+    for (int i=0;i<n;i++) {
+        cout << arr[i] << " ";
+    }
+
+}
